6baba561525d658951cb7fcda15be929e284b478
Simon Meskens <meskens.simon@gmail.com>
2024-06-27 00:51:18 +0200

    Update 1

--- install.sh
+++ install.sh
@@ -2,24 +2,13 @@
 
 #== Setup ==#
     echo "Updating package list..."
-    sudo apt-get update -y
+    sudo apt update -y
     
     echo "Upgrading distro..."
     sudo apt dist-upgrade -y
     
     echo "Upgrading existing packages..."
-    sudo apt-get upgrade -y
-
-    echo "Copying over dotfiles..."
-    sudo apt install -y rsync
-    rsync -av ./home ~
-
-    echo "Adding Microsoft repository..."
-    sudo apt install -y wget gpg
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-    rm -f packages.microsoft.gpg
+    sudo apt upgrade -y
 
 #== Remove ==#
     echo "Removing unnecessary packages"
@@ -27,10 +16,31 @@
 
 #== Install ==#
     echo "Installing new packages..."
+    sudo apt install -y rsync wget gpg
+
+    #-- Dotfiles --#
+    echo "Copying over dotfiles..."
+    rsync -av ./home/ ~
 
     #-- Keyring tools --#
-    sudo apt install -y libsecret-tool seahorse
-    #TODO set up default keyring
+    sudo apt install -y libsecret-1-0 libsecret-1-dev libsecret-tools seahorse
+    echo "auth optional pam_gnome_keyring.so" | sudo tee -a /etc/pam.d/common-auth > /dev/null
+    echo "session optional pam_gnome_keyring.so auto_start" | sudo tee -a /etc/pam.d/common-session > /dev/null
+    echo "password optional pam_gnome_keyring.so" | sudo tee -a /etc/pam.d/passwd > /dev/null
+
+    echo "[Unit]
+Description=GNOME Keyring Daemon
+
+[Service]
+ExecStart=gnome-keyring-daemon --components=secrets,ssh,pkcs11 --unlock --foreground
+KillUserProcesses=no
+
+[Install]
+WantedBy=default.target" | sudo tee /etc/systemd/user/gnome-keyring-daemon.service > /dev/null
+    systemctl --user daemon-reload
+    systemctl --user enable gnome-keyring-daemon.service
+    systemctl --user start gnome-keyring-daemon.service
+    secret-tool store --label="Login Keyring" key-type=login key-value=initial
 
     #-- Terminal apps --#
     sudo apt install -y nano
@@ -47,11 +57,39 @@
     gsettings set org.gnome.desktop.interface gtk-theme "Orchis-Dark"
     gsettings set org.gnome.desktop.wm.preferences theme "Orchis-Dark"
 
+#== Dev tools ==#
+
+    echo "Installing dev tools..."
+    sudo apt install -y build-essential
+
     #-- Git --#
+    echo "Configuring git..."
+    sudo apt install -y git-credential-oauth
+
     git config --global init.defaultBranch main
 
+    git config --global --unset-all credential.helper
+    git config --global --add credential.helper "cache --timeout 7200" # two hours
+    git config --global --add credential.helper oauth
+    git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
+    read -p "Enter your email address for Git: " email
+    read -p "Enter your Git username: " username
+
+    git config --global user.email "$email"
+    git config --global user.name "$username"
+
     #-- IDE --#
+    echo "Adding Microsoft repository..."
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    rm -f packages.microsoft.gpg
+
+    sudo apt update -y
     sudo apt install -y code
+    
+    #-- Mise --#
 
 #== Flatpak ==#
     #TODO make this all quiet
@@ -65,4 +103,8 @@
     sudo apt autoremove -y
     sudo apt clean
 
-    echo "\nPlease restart the Linux instance to finish..."
+    echo "Restarting Sommelier..."
+    systemctl --user daemon-reload
+    systemctl --user restart sommelier-x@0.service
+
+    echo -e "\nPlease restart the Linux instance to finish..."
